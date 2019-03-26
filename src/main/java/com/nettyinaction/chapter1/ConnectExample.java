e8129f772d3184f5da0adffe67f7a7929735b84c
zhang.ling <zl@163.com>
2019-03-27 01:31:23 +0800

    fix

---
+++ src/main/java/com/nettyinaction/chapter1/ConnectExample.java
@@ -0,0 +1,10 @@
+package com.nettyinaction.chapter1;
+
+/**
+ * Created by zl239.
+ *
+ */
+public class ConnectExample {
+    //private static final Channel CHANEL_FROM_SOMEWHERE = new NioSocketChannel();
+
+}
